25edb15d1e1ec7d86cbfbf12e063f5e7ea14f9b2
saotrafahasoavana <saotraandrianome78@gmail.com>
2024-07-28 21:44:17 +0300

    create_and_join_threads

---
+++ includes/philo.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: saandria <saandria@student.42antananari    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/28 20:18:31 by saandria          #+#    #+#             */
+/*   Updated: 2024/07/28 20:20:12 by saandria         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+
+# define PHILO_H
+
+# include <pthread.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <unistd.h>
+
+#endif
---
+++ src/threads.c
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   threads.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: saandria <saandria@student.42antananari    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/28 20:14:48 by saandria          #+#    #+#             */
+/*   Updated: 2024/07/28 21:27:32 by saandria         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/philo.h"
+
+void	*to_do(void *id)
+{
+	long	tid;
+
+	tid = (long)id;
+	printf("thread %ld starts\n", tid);
+	sleep(1);
+	printf("thread %ld ends\n", tid);
+	pthread_exit(NULL);
+}
+
+int	main(int ac, char *av[])
+{
+	pthread_t	*threads;
+	long			t_num;
+	long			i;
+
+	if (ac > 1)
+		t_num = atoi(av[1]);
+	else
+		return (0);
+	threads = malloc(sizeof(pthread_t) * (t_num));
+	i = 0;
+	while (i < t_num)
+	{
+		printf("\033[1;6mCreating thread %ld\033[0m\n", i + 1);
+		pthread_create(&threads[i], NULL, to_do, (void *)(i + 1));
+		pthread_join(threads[i], NULL);
+		i++;
+	}
+	printf("\033[1;93mCOMPLETED\033[0m\n");
+	free(threads);
+	pthread_exit(NULL);
+}
