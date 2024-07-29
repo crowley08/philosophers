e83934a528851d9496016b013874519811c3d5aa
Saotra Fahasoavana Andrianome <saandria@c1r7s8.42antananarivo.mg>
2024-07-29 14:39:35 +0300

    ph_routine

---
+++ src/routine.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   routine.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: saandria <saandria@student.42antananari    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/29 13:11:41 by saandria          #+#    #+#             */
+/*   Updated: 2024/07/29 14:36:41 by saandria         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/philo.h"
+
+void	*to_do(void *p)
+{
+	t_philo	*tid;
+
+	tid = (t_philo *)p;
+	printf("\033[1;12m%d is \033[0m", tid->id);
+	ph_sleep(tid);
+	printf("%s\n\033[0m", tid->status);
+	sleep(3);
+	return (NULL);
+}
+
+void	*ph_think(t_philo *p)
+{
+	p->status = "\033[1;93m\033[1;6mthinking...";
+}
+
+void	*ph_sleep(t_philo *p)
+{
+	p->status = "\033[1;92m\033[1;6msleeping...";
+}
+
+void	*ph_eat(t_philo *p)
+{
+	p->status = "\033[1;95m\033[1;6meating...";
+}
+
+void	*ph_died(t_philo *p)
+{
+	p->status = "\033[1;91m\033[1;6mdied...";
+}
--- src/threads.c
+++ src/threads.c
@@ -6,44 +6,36 @@
 /*   By: saandria <saandria@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/28 20:14:48 by saandria          #+#    #+#             */
-/*   Updated: 2024/07/29 10:44:51 by saandria         ###   ########.fr       */
+/*   Updated: 2024/07/29 14:36:44 by saandria         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/philo.h"
 
-void	*to_do(void *id)
-{
-	long	tid;
-
-	tid = (long)id;
-	printf("thread %ld starts\n", tid);
-	printf("\033[1;6mloading.....\n\033[0m");
-	sleep(3);
-	printf("thread %ld ends\n", tid);
-	pthread_exit(NULL);
-}
-
 int	main(int ac, char *av[])
 {
-	pthread_t	*threads;
-	long			t_num;
-	long			i;
+	t_philo			p;
+	int			t_num;
+	int			i;
 
 	if (ac > 1)
-		t_num = atol(av[1]);
+		t_num = atoi(av[1]);
 	else
 		return (0);
-	threads = malloc(sizeof(pthread_t) * (t_num));
+	p.threads = malloc(sizeof(pthread_t) * (t_num));
+	p.id = 1;
 	i = 0;
-	while (i < t_num)
+	while (1)
 	{
-		printf("\033[1;93mCreating thread %ld\033[0m\n", i + 1);
-		pthread_create(&threads[i], NULL, to_do, (void *)(i + 1));
-		pthread_join(threads[i], NULL);
-		i++;
+		while (i < t_num)
+		{
+			pthread_create(&p.threads[i], NULL, to_do, &p);
+			pthread_join(p.threads[i], NULL);
+			i++;
+			p.id++;
+		}
 	}
 	printf("\033[1;91mCOMPLETED\033[0m\n");
-	free(threads);
+	free(p.threads);
 	pthread_exit(NULL);
 }
