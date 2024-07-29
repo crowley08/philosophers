5239b9ff328e52849355d78c7f4432d70870d8fa
Saotra Fahasoavana Andrianome <saandria@c1r7s8.42antananarivo.mg>
2024-07-29 14:40:37 +0300

    struct

--- includes/philo.h
+++ includes/philo.h
@@ -6,7 +6,7 @@
 /*   By: saandria <saandria@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/28 20:18:31 by saandria          #+#    #+#             */
-/*   Updated: 2024/07/28 22:12:43 by saandria         ###   ########.fr       */
+/*   Updated: 2024/07/29 14:36:59 by saandria         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,5 +18,26 @@
 # include <stdlib.h>
 # include <stdio.h>
 # include <unistd.h>
+
+typedef	struct s_philo
+{
+	pthread_t	*threads;
+	char		*status;
+	int			id;
+	int			forks;
+}				t_philo;
+
+/*
+typedef	struct s_table
+{
+	t_philo	*ph;
+}			t_table;
+*/
+
+void	*to_do(void *p);
+void	*ph_think(t_philo *p);
+void	*ph_eat(t_philo *p);
+void	*ph_sleep(t_philo *p);
+void	*ph_died(t_philo *p);
 
 #endif
