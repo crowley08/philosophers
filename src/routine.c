a8541f8a3261d67bb9f65f3eeea57b12d56459ba
Saotra Fahasoavana Andrianome <saandria@c1r9s8.42antananarivo.mg>
2024-08-03 04:40:17 +0300

    rectif

--- src/routine.c
+++ src/routine.c
@@ -6,7 +6,7 @@
 /*   By: saandria <saandria@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/29 13:11:41 by saandria          #+#    #+#             */
-/*   Updated: 2024/08/03 04:03:31 by saandria         ###   ########.fr       */
+/*   Updated: 2024/08/03 04:38:44 by saandria         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,24 +33,24 @@ void	*to_do(void *p)
 
 void	ph_think(t_philo *p)
 {
-	p->stat = "\033[1;93mthinking...";
-	printf("\033[1;3m%lld  %d is %s\n\033[0m", p->ta->ts, p->id, p->stat);
+	p->stat = "\033[1;93m is thinking...";
+	printf("\033[1;3m%lld  %d %s\n\033[0m", p->ta->ts * 1000, p->id, p->stat);
 }
 
 void	ph_sleep(t_philo *p)
 {
-	p->stat = "\033[1;92msleeping...";
-	printf("\033[1;3m%lld  %d is %s\n\033[0m", p->ta->ts, p->id, p->stat);
+	p->stat = "\033[1;92mis sleeping...";
+	printf("\033[1;3m%lld  %d %s\n\033[0m", p->ta->ts * 1000, p->id, p->stat);
 	usleep(p->t.to_sleep * 1000);
 }
 
 void	ph_eat(t_philo *p)
 {
 	pthread_mutex_lock(&p->mutex);
-	p->stat = "\033[1;95meating...";
-	printf("\033[1;3m%lld  %d is %s\n\033[0m", p->ta->ts, p->id, p->stat);
+	p->stat = "\033[1;95mis eating...";
+	printf("\033[1;3m%lld  %d %s\n\033[0m", p->ta->ts * 1000, p->id, p->stat);
 	p->eaten++;
-	printf("%lld  %d ate %d times\n", p->ta->ts, p->id, p->eaten);
+	printf("%lld  %d ate %d times\n", p->ta->ts * 1000, p->id, p->eaten);
 	usleep(p->t.to_eat * 1000);
 	pthread_mutex_unlock(&p->mutex);
 }
@@ -58,6 +58,6 @@ void	ph_eat(t_philo *p)
 void	ph_died(t_philo *p)
 {
 	p->ta->ts++;
-	p->stat = "\033[1;91mdied...";
-	printf("\033[1;3m%lld  %d is %s\n\033[0m", p->ta->ts, p->id, p->stat);
+	p->stat = "\033[1;91mis died...";
+	printf("\033[1;3m%lld  %d %s\n\033[0m", p->ta->ts * 1000, p->id, p->stat);
 }
