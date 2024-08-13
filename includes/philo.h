d7ea68d80bf8cfdb0ddb53b8432ab2a9fb0c611c
Saotra Fahasoavana Andrianome <saandria@c1r9s15.42antananarivo.mg>
2024-08-13 16:02:10 +0300

    tri .h

--- includes/philo.h
+++ includes/philo.h
@@ -6,7 +6,7 @@
 /*   By: saandria <saandria@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/28 20:18:31 by saandria          #+#    #+#             */
-/*   Updated: 2024/08/13 15:35:58 by saandria         ###   ########.fr       */
+/*   Updated: 2024/08/13 16:01:40 by saandria         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -58,36 +58,45 @@ typedef struct s_table
 	int				everyone_ate;
 }					t_table;
 
-void		*to_do(void *p);
-void		ph_think(t_philo *p);
-void		ph_eat(t_philo *p);
-void		ph_sleep(t_philo *p);
+//ph_functions
 int			ph_atoi(char *n);
 long		ph_atol(char *n);
-void		init_time(t_philo *p, char *av[]);
-void		print_stat(t_philo *p);
-void		take_forks(t_philo *p);
-void		release_forks(t_philo *p);
-void		init_table(t_table *table, char *av[]);
-long long	get_time(void);
 void		ph_threads(t_table *table, char *av[]);
+void		ph_ate(t_table *table);
+int			ph_isdigit(char *av[]);
+int			ph_error(char *error);
+void		ph_usleep(long ts, t_table *table);
+
+//init_functions
+void		init_time(t_philo *p, char *av[]);
+void		init_table(t_table *table, char *av[]);
 void		init_forks(t_table *table);
 void		*init_threads(t_table *table, char *av[]);
+
+//simulation
+void		*to_do(void *p);
+void		*ph_monitor(void *t);
+void		ph_think(t_philo *p);
+void		take_forks(t_philo *p);
+void		ph_eat(t_philo *p);
+void		release_forks(t_philo *p);
+void		ph_sleep(t_philo *p);
+
+//utils
+void		print_stat(t_philo *p);
+long long	get_time(void);
+void		clean_threads(t_table *table);
+int			check_arg(char *s);
+void		free_all(t_table *table);
+void		*clear(t_table *table, char *s);
 void		*join_threads(t_table *table);
 void		destroy_mutex(t_table *table);
+
+//ending_conditions
 void		is_dead(t_table *table);
 int			no_one_died(t_table *table);
 int			check_death(t_table *table, int i);
-void		*ph_monitor(void *t);
-void		clean_threads(t_table *table);
-void		ph_usleep(long ts, t_table *table);
-void		free_all(t_table *table);
-void		*clear(t_table *table, char *s);
 int			check_ate(t_table *table);
-void		ph_ate(t_table *table);
 int			someone_didn_t_eat_yet(t_table *table);
-int			check_arg(char *s);
-int			ph_isdigit(char *av[]);
-int			ph_error(char *error);
 
 #endif
